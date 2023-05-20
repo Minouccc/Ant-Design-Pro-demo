93f6187161e3d4e277266e6e5f6f8b90a72360f6
Minouccc <343723899@qq.com>
2023-05-20 22:48:54 +0800

    feat: processing form submission process

--- src/pages/BasicList/builder/ActionBuilder.tsx
+++ src/pages/BasicList/builder/ActionBuilder.tsx
@@ -1,11 +1,20 @@
 import { Button } from 'antd';
 import { ButtonType } from 'antd/es/button/buttonHelpers';
 import { BasicListApi } from '../data';
-const ActionBuilder = (actions: BasicListApi.Action[] | undefined) => {
+const ActionBuilder = (
+  actions: BasicListApi.Action[] | undefined,
+  actionHandler: (action: BasicListApi.Action) => void,
+) => {
   return (actions || []).map((action) => {
     if (action.component === 'button') {
       return (
-        <Button key={action.text} type={action.type as ButtonType}>
+        <Button
+          key={action.text}
+          type={action.type as ButtonType}
+          onClick={() => {
+            actionHandler(action);
+          }}
+        >
           {action.text}
         </Button>
       );
--- src/pages/BasicList/component/Modal.tsx
+++ src/pages/BasicList/component/Modal.tsx
@@ -1,9 +1,10 @@
-import { Form, Modal as AntdModal } from 'antd';
+import { Form, Input, Modal as AntdModal } from 'antd';
 import moment from 'moment';
 import { useEffect } from 'react';
 import { useRequest } from 'umi';
 import ActionBuilder from '../builder/ActionBuilder';
 import FormBuilder from '../builder/FormBuilder';
+import { BasicListApi } from '../data';
 const Modal = ({
   isModalOpen,
   hideMoal,
@@ -14,7 +15,27 @@ const Modal = ({
   modalUrl: string;
 }) => {
   const [form] = Form.useForm();
-  const init = useRequest<{ data: PageApi.Data }>(`${modalUrl}`);
+  const init = useRequest<{ data: PageApi.Data }>(`${modalUrl}`, {
+    manual: true,
+  });
+  const request = useRequest(
+    (values: any) => {
+      const { uri, method, ...formValues } = values;
+      return {
+        url: `https://public-api-v2.aspirantzhang.com${uri}`,
+        method,
+        data: {
+          ...formValues,
+          'X-API-KEY': 'antd',
+          create_time: moment(formValues.create_time).format(),
+          update_time: moment(formValues.update_time).format(),
+        },
+      };
+    },
+    {
+      manual: true,
+    },
+  );
   const layout = {
     labelCol: { span: 8 },
     wrapperCol: { span: 16 },
@@ -39,6 +60,20 @@ const Modal = ({
     }
     return {};
   };
+  const onFinish = (values: any) => {
+    request.run(values);
+    init.run();
+  };
+  const actionHandler = (action: BasicListApi.Action) => {
+    switch (action.action) {
+      case 'submit':
+        form.setFieldsValue({ uri: action.uri, method: action.method });
+        form.submit();
+        break;
+      default:
+        break;
+    }
+  };
   useEffect(() => {
     if (isModalOpen) {
       form.resetFields();
@@ -56,7 +91,7 @@ const Modal = ({
         title={init?.data?.page?.title}
         open={isModalOpen}
         onCancel={hideMoal}
-        footer={ActionBuilder(init?.data?.layout?.actions[0]?.data)}
+        footer={ActionBuilder(init?.data?.layout?.actions[0]?.data, actionHandler)}
         maskClosable={false}
       >
         <Form
@@ -67,8 +102,15 @@ const Modal = ({
             update_time: moment(),
             status: true,
           }}
+          onFinish={onFinish}
         >
           {FormBuilder(init?.data?.layout?.tabs[0]?.data)}
+          <Form.Item name="uri" key="uri" hidden>
+            <Input />
+          </Form.Item>
+          <Form.Item name="method" key="method" hidden>
+            <Input />
+          </Form.Item>
         </Form>
       </AntdModal>
     </>
